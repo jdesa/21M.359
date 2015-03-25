e237cda896eaa0829a5694dbff011d56c9207918
Jacqui De Sa <jdesa@mit.edu>
2015-03-25 10:18:05 -0700

    Assignment 6: Finished Part 1

--- Assignment6/code/pset6.py
+++ Assignment6/code/pset6.py
@@ -12,6 +12,8 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Line
 from kivy.graphics.instructions import InstructionGroup
 
+from pprint import pprint
+
 import numpy as np
 
 
@@ -34,27 +36,36 @@ class NotePlayer(object):
 class String(InstructionGroup):
    def __init__(self, bottom_pt, top_pt, rgb):
       super(String, self).__init__()
-
+      self.bottom_pt = bottom_pt
+      self.top_pt = top_pt
+      self.mid_pt = (bottom_pt[0], (top_pt[1] - bottom_pt[1])/2 + bottom_pt[1])
+      self.string = Line(points = [self.top_pt[0], self.top_pt[1], self.mid_pt[0], self.mid_pt[1], self.bottom_pt[0], self.bottom_pt[1]], width = 5)
+      self.active = True
+      self.color = Color(255,255,255)
 
    # bends the string by moving the mid-point of the string to the "hand
    # location"
    def set_mid_point(self, x, y):
-      pass
+      if self.active:
+         self.mid_pt = (x,y)
 
    # resets the string to being normal / still.
    def set_inactive(self):
-      pass
+      self.active = False
 
    # useful for the gesture to know what the resting x-position of the string
    # is
    def get_resting_x(self):
-      return 0
+      return self.top_pt[0]
+
+   def get_mid_point_height(self):
+      return (self.top_pt[1] - self.bottom_pt[1])/2 + self.bottom_pt[1]
 
    # if the string is going to animate (say, when it is plucked), on_update is
    # necessary
-   def on_update(self, dt):
-      pass
-
+   def on_update(self):
+      self.string = Line(points = [self.top_pt[0], self.top_pt[1], self.mid_pt[0], self.mid_pt[1], self.bottom_pt[0], self.bottom_pt[1]], width = 5)
+      return self.string
 
 # This class monitors the location of the hand and determines if a pluck of
 # the string happened. Each PluckGesture is associated with a string (1-1
@@ -77,11 +88,24 @@ class Harp(InstructionGroup):
       self.pos = pos
       self.num_strings = num_strings
       self.cursor = Cursor3D(area, pos, (.2, .6, .2), True)
+      self.z_index = 0
+      self.active = False
+      self.active_color = (0,255,0)
+      self.inactive_color = (255, 0, 0)
+
+      self.string = String((area[0]/2+pos[0], pos[1]), (area[0]/2+pos[0], area[1] + pos[1]), (0,0,255))
 
 
    # set the hand position as a 3D vector ranging from [0,0,0] to [1,1,1]
    def set_hand_pos(self, pos):
       self.cursor.set_pos(pos)
+      self.z_index = pos[2]
+      self.active = True if self.z_index <= .5 else False
+      color = self.active_color if self.active else self.inactive_color
+      self.cursor.set_color(color)
+
+      print "pos: " + str(pos)
+      print "mid_pt: " + str(self.string.mid_pt)
 
 
    # callback to be called from a PluckGesture when a pluck happens
@@ -91,7 +115,6 @@ class Harp(InstructionGroup):
    # for animation and NotePlayer
    def on_update(self, dt):
       pass
-
 
 kUseKinect = False
 
@@ -115,7 +138,11 @@ class MainWidget(BaseWidget) :
       pos = (100, 100) #Harp position
       self.harp = Harp(area, pos, 10) #harp initalization, 10 strings
       self.canvas.add(self.harp)
+      self.canvas.add(Color(255,255,255))
+      self.string = self.harp.string.string
+      self.canvas.add(self.string)
       self.canvas.add(self.harp.cursor)
+
 
    def on_update(self) :
       if kUseKinect:
@@ -127,7 +154,7 @@ class MainWidget(BaseWidget) :
          norm_pt = scale_point(pt, pt_min, pt_max)
 
       else:
-         pt = [Window.mouse_pos[0], Window.mouse_pos[1], 0]
+         pt = [Window.mouse_pos[0], Window.mouse_pos[1], 0.5]
          if 'shift' in self.down_keys:
             pt[2] = 0.5
 
@@ -140,8 +167,15 @@ class MainWidget(BaseWidget) :
       
       self.harp.set_hand_pos(norm_pt)
 
+      if self.harp.active and abs(pt[0] - self.harp.string.get_resting_x()) < 50 and abs(pt[1] - self.harp.string.get_mid_point_height()) < 50:
+         self.harp.string.set_mid_point(pt[0], pt[1])
+         print "grabbed"
+         self.canvas.remove(self.string)
+         self.string = self.harp.string.on_update()
+         self.canvas.add(self.harp.string.color)
+         self.canvas.add(self.string)
 
-
+         print "self.string is: " + str(self.string)
 
 # convert pt into unit scale (ie, range [0,1]) assuming that pt falls in the
 # the range [min_val, max_val]
