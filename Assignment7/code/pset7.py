bc859bfbee957321db647615ee713fd1db17987b
Jacqui De Sa <jdesa@mit.edu>
2015-04-12 12:10:51 -0400

    Commit before implementing gem drawing optimization.

--- Assignment7/code/pset7.py
+++ Assignment7/code/pset7.py
@@ -44,27 +44,29 @@ class MainWidget(BaseWidget) :
       self.beatmatchdisplay = BeatMatchDisplay(self.gemdata, self.audiocontroller.song.cond)
       self.canvas.add(self.beatmatchdisplay)
 
+      self.player = Player(self.gemdata, self.beatmatchdisplay, self.audiocontroller)
+
    def on_key_down(self, keycode, modifiers):
       # play / stop toggle
       if keycode[1] == 'p':
          self.audiocontroller.toggle()
 
       if keycode[1] == 't':
-         #print(self.gemdata.get_all_ticks())
+         ##print(self.gemdata.get_all_ticks())
          pass
       if keycode[1] == 'spacebar':
          self.audiocontroller.set_mute(True)
 
-      if keycode[1] == "a":
-         self.beatmatchdisplay.nowbar[0].on_down(True)
-      if keycode[1] == "s":
-         self.beatmatchdisplay.nowbar[1].on_down(True)
-      if keycode[1] == "d":
-         self.beatmatchdisplay.nowbar[2].on_down(True)
-      if keycode[1] == "f":
-         self.beatmatchdisplay.nowbar[3].on_down(True)
-      if keycode[1] == "g":
-         self.beatmatchdisplay.nowbar[4].on_down(True)
+      if keycode[1] == "a" or keycode[1] == "1":
+         self.player.on_button_down(0)
+      if keycode[1] == "s" or keycode[1] == "2":
+         self.player.on_button_down(1)
+      if keycode[1] == "d" or keycode[1] == "3":
+         self.player.on_button_down(2)
+      if keycode[1] == "f" or keycode[1] == "4":
+         self.player.on_button_down(3)
+      if keycode[1] == "g" or keycode[1] == "5":
+         self.player.on_button_down(4)
 
       # button down   
       idx = '12345'.find(keycode[1])
@@ -76,18 +78,19 @@ class MainWidget(BaseWidget) :
       idx = '12345'.find(keycode[1])
       if idx != -1:
          pass
-      if keycode[1] == "a":
-         self.beatmatchdisplay.nowbar[0].on_up()
-      if keycode[1] == "s":
-         self.beatmatchdisplay.nowbar[1].on_up()
-      if keycode[1] == "d":
-         self.beatmatchdisplay.nowbar[2].on_up()
-      if keycode[1] == "f":
-         self.beatmatchdisplay.nowbar[3].on_up()
-      if keycode[1] == "g":
-         self.beatmatchdisplay.nowbar[4].on_up()
+      if keycode[1] == "a" or keycode[1] == "1":
+         self.player.on_button_up(0)
+      if keycode[1] == "s" or keycode[1] == "2":
+         self.player.on_button_up(1)
+      if keycode[1] == "d" or keycode[1] == "3":
+         self.player.on_button_up(2)
+      if keycode[1] == "f" or keycode[1] == "4":
+         self.player.on_button_up(3)
+      if keycode[1] == "g" or keycode[1] == "5":
+         self.player.on_button_up(4)
 
    def on_update(self) :
+      self.player.on_update()
       self.beatmatchdisplay.on_update()
 
 # creates the Audio driver
@@ -104,7 +107,7 @@ class AudioController(object):
       # set up Tempo Track correctly.
       self.tempo_map = TempoMap("_takemeout_tempo.txt")
       self.song.cond.set_tempo_map(self.tempo_map)
-      print (str(self.song.cond.tempo_map))
+      ###print(str(self.song.cond.tempo_map))
       
       #add background track
       self.bg_track = AudioTrack(self.audio, self.song_name + "_bg.wav")
@@ -158,7 +161,7 @@ class BarLineDisplay(InstructionGroup):
    def __init__(self, tick):
       super(BarLineDisplay, self).__init__()
       self.tick = tick
-      self.line = Rectangle(pos=(0, tick), size=(800, 1))
+      self.line = Rectangle(pos=(0, tick + gem_height/2), size=(800, 1))
       self.add(Color(255,255,255))
       self.add(self.line)
 
@@ -198,7 +201,7 @@ class GemData(object):
                   self.gems.append(gem)
                   self.ticks.append(tick)
                   tick += duration
-               #print "tick is: " + str(tick)
+               ###print"tick is: " + str(tick)
 
    def get_all_gems(self):
       return self.gems
@@ -208,8 +211,20 @@ class GemData(object):
 
    # return a sublist of the gems that match this time slice:
    def get_gems_in_range(self, start_tick, end_tick):
-      gems_in_range = [x for x in self.gems if (start_tick <= x and x <= end_tick)]
+      gems_in_range = []
+      for gem in self.gems:
+         gem_tick = gem.get_tick()
+         if start_tick <= gem_tick and gem_tick <= end_tick:
+            gems_in_range.append(gem)
       return gems_in_range
+
+   def get_gems_in_range_on_track(self, start_tick, end_tick, track):
+      gems_in_range = []
+      for gem in self.gems:
+         gem_tick = gem.get_tick()
+         if start_tick <= gem_tick and gem_tick <= end_tick and gem.track == track:
+            gems_in_range.append(gem)
+      return gems_in_range      
 
    def get_len(self):
       return len(self.gems)
@@ -219,19 +234,30 @@ class GemDisplay(InstructionGroup):
    def __init__(self, pos, color):
       super(GemDisplay, self).__init__()
       self.color = Color(*color)
-      self.color.a = .75
+      self.color.a = .5
       self.pos = pos
       self.rectangle = Rectangle(pos=pos, size=(track_width, gem_height))
+      self.triggered = False
       self.add(self.color)
       self.add(self.rectangle)
+
 
    # change to display this gem being hit
    def on_hit(self):
       self.color.a = 1
+      self.triggered = True
 
    # change to display a passed gem
    def on_pass(self):
-      self.color = .5
+      ##print"hasn't been hit"
+      #print "gemdisplay hasn't been hit"
+      self.color.rgb = (.5,.5,.5)
+      self.color.a = .5
+      self.triggered = True
+
+
+   def has_triggered(self):
+      return self.triggered
 
    # useful if gem is to animate
    def on_update(self, dt):
@@ -241,24 +267,36 @@ class GemDisplay(InstructionGroup):
 class ButtonDisplay(InstructionGroup):
    def __init__(self, pos, color):
       super(ButtonDisplay, self).__init__()
+      self.rgb = color
       self.color = Color(*color)
-      self.color.a = .75
+      self.color.a = .5
       self.pos = pos
-      self.rectangle = Rectangle(pos=(pos, 0), size=(track_width, gem_height))
+      self.rectangle = Rectangle(pos=(pos, 100), size=(track_width, gem_height))
       self.add(self.color)
       self.add(self.rectangle)
    # displays when button is down (and if it hit a gem)
-   def on_down(self, hit):
+   
+   def on_button_down(self):
       self.color.a = 1
+      self.color.rgb = self.rgb
+
+
+   def on_down(self, hit):
       if hit:
-         print "Hit a gem."
+         self.color.rgb = (1,1,1)
+         self.color.a = 1
+         ###print"Hit a gem."
       else:
-         print "Did not hit a gem."
+         self.color.rgb = (.5,.5,.5)
+         self.color.a = .5
+         ###print"Did not hit a gem."
 
    # back to normal state
    def on_up(self):
-      self.color.a = .75
-      print "Button on up" 
+      print "on_up being called"
+      self.color.rgb = self.rgb
+      self.color.a = .5
+      ###print"Button on up"    
 
 # Displays all game elements: Nowbar, Buttons, BarLines, Gems.
 # scrolls the gem display.
@@ -267,6 +305,7 @@ class BeatMatchDisplay(InstructionGroup):
    def __init__(self, gem_data, cond):
       super(BeatMatchDisplay, self).__init__()
       self.cond = cond
+      self.gem_data = gem_data
 
       #Button Creation
       self.nowbar = []
@@ -276,7 +315,7 @@ class BeatMatchDisplay(InstructionGroup):
          self.add(button)
 
       self.translate = Translate(0,0)
-      print "translate: " + str(self.translate)
+      ##print"translate: " + str(self.translate)
       self.add(self.translate)
 
       #Gem Creation
@@ -284,55 +323,131 @@ class BeatMatchDisplay(InstructionGroup):
 
       #Barline Creation
       max_tick  = self.gem_data.get_all_gems()[-1].get_tick()
-      print "max_tick is: " + str(max_tick)
+      ###print"max_tick is: " + str(max_tick)
       for tick in range(0, max_tick, kticksperquarter):
          bar = BarLineDisplay(tick - kticksperquarter/2)
          self.add(bar)
+
+      self.gemdisplaydict = {}
 
       for gem in self.gem_data.get_all_gems():
          pos = (gem.track_to_posx(), gem.tick_to_posy())
          color = color_dict[gem.track]
          gemdisplay = GemDisplay(pos, color)
+         self.gemdisplaydict[gem] = gemdisplay
          self.add(gemdisplay)
       
-
-
    # called by Player. Causes theright thing to happen
-   def gem_hit(self, gem_idx):
-      pass
+   def gem_hit(self, gem):
+      ##print"in gem_hit!"
+      self.gemdisplaydict[gem].on_hit()
 
    # called by Player. Causes the right thing to happen
-   def gem_pass(self, gem_idx):
-      pass
+   def gem_pass(self, gem):
+      ##print"in gem_pass!"
+      self.gemdisplaydict[gem].on_pass()
+      self.nowbar[gem.track].on_down(False)
 
    # called by Player. Causes the right thing to happen
    def on_button_down(self, lane, hit):
-      pass
+      ##print"on_button_down from beatmatchdisplay"
+      self.nowbar[lane].on_down(hit)
 
    # called by Player. Causes the right thing to happen
    def on_button_up(self, lane):
-      pass
+      self.nowbar[lane].on_up()
 
    # call every frame to make gems and barlines flow down the screen
    def on_update(self) :
       self.translate.y = - (self.cond.get_tick()/4.0)
+
+   def draw_gems(drawable_gems):
+      non_drawable_gems = [gem for gem in self.gem_data.get_all_gems() if not gem in drawable_gems]
+
+      for gems in drawable_gems
 
 # Handles game logic and keeps score. 
 # Controls the display and the audio
 class Player(object):
    def __init__(self, gem_data, display, audio_ctrl):
       super(Player, self).__init__()
+      self.gemdata = gem_data
+      self.beatmatchdisplay = display
+      self.audio_ctrl = audio_ctrl
+      self.score = 0
 
    # called by MainWidget
    def on_button_down(self, lane) :
-      pass
+      print "player on button down"
+
+      self.beatmatchdisplay.nowbar[lane].on_button_down()
+
+
+      current_tick = self.audio_ctrl.song.cond.get_tick()
+      lower_tick_bound = (current_tick)/4 - 50 + 150
+      upper_tick_bound = (current_tick)/4 + 50 + 150
+      possible_gem_list = self.gemdata.get_gems_in_range_on_track(lower_tick_bound, upper_tick_bound, lane)      
+      if len(possible_gem_list) == 0:
+         pass
+         ##print"lower_tick_bound: " + str(lower_tick_bound)
+         ##print"upper_tick_bound: " + str(upper_tick_bound)
+         ##print"lane: " + str(lane)
+         ###printself.gemdata.get_all_gems()
+      elif len(possible_gem_list) == 1:
+
+         ##print"lower_tick_bound: " + str(lower_tick_bound)
+         ##print"upper_tick_b ound: " + str(upper_tick_bound)
+         ##print"lane: " + str(lane)
+         ##print"hit gem"
+         self.beatmatchdisplay.gem_hit(possible_gem_list[0])
+      else:
+         pass
+         ##print"multiple gems in range"
 
    # called by MainWidget
    def on_button_up(self, lane):
-      pass
+      print "player on button up"
+      #self.button_up()
+      self.beatmatchdisplay.on_button_up(lane)
 
    # needed to check if for pass gems (ie, went past the slop window)
-   def on_update(self) :
-      pass
+   def on_update(self):
+      #print "in on_update"
+      self.draw_gems()
+      older_tick = self.audio_ctrl.song.cond.get_tick()
+      #print "older tick is: " + str(older_tick)
+      lower_tick_bound = (older_tick)/4 - 50 + 50
+      upper_tick_bound = (older_tick)/4 + 50 + 50
+      possible_gem_list = []
 
+      for lane in range(5):
+         possible_gem_list += self.gemdata.get_gems_in_range_on_track(lower_tick_bound, upper_tick_bound, lane)      
+         
+      untouched_gems = []
+      for gem in possible_gem_list:
+         gemdisplay = self.beatmatchdisplay.gemdisplaydict[gem]
+         #print "gemdisplay_has_triggered():" + str(gemdisplay.has_triggered())
+         if not(gemdisplay.has_triggered()):
+            untouched_gems.append(gem)
+
+      if len(untouched_gems) == 0:
+         #print"no hit gems"
+         pass
+      else:
+         #print "some non-hit gems"
+         for gem in untouched_gems:
+            self.beatmatchdisplay.gem_pass(gem)
+
+   def draw_gems(self):
+      current_tick = self.audio_ctrl.song.cond.get_tick()
+      top_of_screen = (current_tick)/4 - 1000
+      bottom_of_screen = (current_tick)/4 + 50
+
+      drawable_gems = []
+      for lane in range(5):
+         drawable_gems += self.gemdata.get_gems_in_range_on_track(top_of_screen, bottom_of_screen, lane)
+
+      self.beatmatchdisplay.draw_gems(drawable_gems)   
+
+      
 run(MainWidget)
